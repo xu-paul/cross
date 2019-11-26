6875dd5a996763a5dff3f52cacc3446ccaedd046
北风 <442603489@qq.com>
2019-11-26 13:40:48 +0800

    cross

---
+++ build/precache-manifest.a655173ac599a37e7ef14aa49a32c13a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3a34718d17547b91728585cc48ed73d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "04e7f5a1adba82d20175",
+    "url": "./static/css/main.67d839ee.chunk.css"
+  },
+  {
+    "revision": "3e3870169509713ca8c9",
+    "url": "./static/js/2.0b3775f9.chunk.js"
+  },
+  {
+    "revision": "04e7f5a1adba82d20175",
+    "url": "./static/js/main.66c85fb5.chunk.js"
+  },
+  {
+    "revision": "ef94158559c9f8cc2e88",
+    "url": "./static/js/runtime-main.c769b6fb.js"
+  }
+]);
---
+++ src/Square.js
@@ -0,0 +1,16 @@
+import React from 'react';
+class Square extends React.Component{ 
+    constructor(){
+        super();
+        this.state = {
+            value:null
+        }
+    }
+    render(){
+        return(
+            <button className="square" onClick={this.props.onClick}>{this.props.value}
+            </button>
+        )
+    }
+}
+export default Square;
